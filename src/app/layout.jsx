7126c9c4c7b36867701cea7e35613335417e7f2d
Runterya <97337724+Runteryaa@users.noreply.github.com>
2026-05-15 17:48:22 +0300

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="bAOoNhpOSqDWF7-4xJ2zJsPtXz6BHK7kEh9_XjLjQ24" />
       <body>{children}</body>
     </html>
   );
